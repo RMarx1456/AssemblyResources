7538a230a2e0d9335cb55d199f234d2a408819b4
RMarx1456 <hellohi12465@gmail.com>
2024-12-09 11:12:05 -0800

    "Finished" this.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AssemblyResources
 Project for SDEV305- will be looking to expand this later on after the project is "finished".
+
+I wanted to do something where I could open an assembly program I wrote, that opens another assembly program, attaches itself to it, pulls the SYSCALL data, then updates the DB with it, but I don't have enough time to, and I'm already sleep deprived while working on it, so that isn't great either.
--- app.js
+++ app.js
@@ -4,36 +4,6 @@ const dotenv = require('dotenv').config();
 const path = require('path');
 const PORT = 10001;
 
-//Executable stuff
-const { spawn } = require('child_process');
-const Assembler = '/usr/bin/nasm'
-const AssemblerArgs = ['-f', 'elf64', __dirname + '/executables/init.asm']
-const Linker = '/usr/bin/ld'
-const LinkerArgs = [__dirname + '/executables/init.o', '-o', 'init']
-const initPath = __dirname + '/executables/init'
-
-let init;
-
-let processRowID;
-let execution = false;
-
-let savedData;
-
-let regBuffer = [];
-
-const BufferOffsets = {
-    RAX: 120,
-    RDI: 112,
-    RSI: 104,
-    RDX: 96,
-    R10: 56,
-    R8: 72,
-    R9: 64,
-
-    RET: 80,
-    identifier: 220
-}
-
 
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
@@ -53,111 +23,38 @@ async function connect() {
     }
 }
 
-async function CreateExecutable() {
-    console.log(__dirname);
-    let assembler = spawn(Assembler, AssemblerArgs, { stdio: 'inherit' });
-    assembler.on('spawn', () => {
-        console.log('Assembler Spawned!');
-    });
-    assembler.on('error', (err) => {
-        console.error(err);
-    });
-    assembler.stdout?.on('data', (data) => {
-        console.log(`${data.toString()}`);
-    });
-    assembler.on('close', () => {
-        console.log('Assembler Closed!');
-    });
+async function updateDB(
+    name, RAX, 
+    RDI, RSI, 
+    RDX, R10, 
+    R8, R9) {
+    const query = `
+    INSERT INTO SYSCALL (RAX, SYS_NAME, RDI, RSI, RDX, R10, R8, R9)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE 
+        RAX = VALUES(RAX),
+        SYS_NAME = VALUES(SYS_NAME),
+        RDI = VALUES(RDI),
+        RSI = VALUES(RSI),
+        RDX = VALUES(RDX),
+        R10 = VALUES(R10),
+        R8 = VALUES(R8),
+        R9 = VALUES(R9);
+    `;
 
-    let linker = spawn(Linker, LinkerArgs, { stdio: 'inherit'});
-    linker.on('spawn', () => {
-        console.log('Linker Spawned!');
-    });
-    linker.on('error', (err) => {
-        console.error(err);
-    });
-    linker.stdout?.on('data', (data) => {
-        console.log(`${data.toString()}`);
-    });
-    linker.on('close', () => {
-        console.log('Linker Closed!');
-    });
-}
+    conn = await pool.getConnection();
 
-function setupChild(childProcess) {
-    console.log('Debug flag!');
-    childProcess.stdout.on('data', (data) => {
-        console.log('Testing output');
-        console.log(data.toString());
-        regBuffer.push(data.toString());
-    });
-    childProcess.on('error', (err) => {
-        console.error(err);
-    });
-    childProcess.on('close', (code) => {
-        console.log(code + " Close")
+    const result = await conn.query(query, [RAX, name, RDI, RSI, RDX, R10, R8, R9])
 
-    });
-}
-
-async function createProcessQuery(connection, name, path, args) {
-    console.log(name);
-    console.log(path);
-    console.log(args);
-    try {
-        let result = await connection.query(
-            `INSERT INTO process (name, EXECUTION_PATH, ARGS)
-            VALUES (?, ?, ?)`, [name, path, args]
-        );
-        console.log(result.insertId);
-        processRowID = parseInt(result.insertId);
-        console.log(processRowID +" ROW ID");
-    }
-    catch (err) {
-        console.error(err);
-        throw err;
-    }
-}
-async function syscallQuery(connection, ID, RAX, RDI, RSI, RDX, R10, R8, R9, RET) {
-    try {
-        let result = await connection.query(
-            `INSERT INTO SYSCALL (ID, RAX, RDI, RSI, RDX, R10, R8, R9, RET)
-            VALUES (${ID}, 
-            ${RAX.toString(10)}, ${RDI.toString(10)}, 
-            ${RSI.toString(10)}, ${RDX.toString(10)}, 
-            ${R10.toString(10)}, ${R8.toString(10)}, 
-            ${R9.toString(10)}, ${RET.toString(10)})`
-        );
-    }
-    catch (err) {
-        console.error(err);
-        throw err;
-    }
-}
-async function addSyscall(connection, ID, regsBuffer) {
-    while(true) {
-        regs = regsBuffer.shift();
-        if(regsBuffer.length > 0 && regs.readInt32LE(BufferOffsets.identifier.toString() === 'regs')) {
-            let RAX = regs.readBigUInt64LE(BufferOffsets.RAX);
-            let RDI = regs.readBigUInt64LE(BufferOffsets.RDI);
-            let RSI = regs.readBigUInt64LE(BufferOffsets.RSI);
-            let RDX = regs.readBigUInt64LE(BufferOffsets.RDX);
-            let R10 = regs.readBigUInt64LE(BufferOffsets.R10);
-            let R8 = regs.readBigUInt64LE(BufferOffsets.R8);
-            let R9 = regs.readBigUInt64LE(BufferOffsets.R9);
-            let RET = regs.readBigUInt64LE(BufferOffsets.RET);
     
-        await syscallQuery(regs, RAX, RDI, RSI, RDX, R10, R8, R9, RET);
-        break;
-        }
-        else {
+}
+async function pullSYSCALLS() {
+    const query = `SELECT * FROM SYSCALL`;
+    conn = await pool.getConnection();
 
-            continue;
-        }
-    }
+    const result = await conn.query(query);
 
-
-
+    return result;
 }
 
 const app = express();
@@ -169,45 +66,26 @@ app.set('view engine', 'ejs');
 
 // Home page
 app.get('/', (req, res) => {
-    CreateExecutable();
     res.render('home', { errors: []});
 });
-app.post('/submit', async (req, res) => {
-    const data = req.body;
-    savedData = data;
-    let errors = [];
-    const conn = await connect();
+app.post('/confirm', async (req, res) => {
+    const regs = req.body;
 
-    async function createProcess(conn, processName, processPath, processArgs) {
-        await createProcessQuery(conn, processName, processPath, processArgs);
-        init = spawn(initPath, [], { cwd: path.join(__dirname, '/executables')});
-        setupChild(init);
-        init.stdin.write(processPath + '\n');
-        init.stdin.write(processArgs + '\n');
-        init.stdin.write('\n');
-        init.stdin.end();
-    }
-
-    if(data.name != null && data.path != null && execution == false) {
-        execution = true; //Locking execution
-        let args;
-        if(data.args === undefined) {
-            args = 'NULL'
-        }
-        else {
-            args = data.args;
-        }
-        createProcess(conn, data.name, data.path, args)
-
-        execution = false;
-        res.render('home', { errors: []});
-    }
-    else {
-
-    }
+    await updateDB(
+        regs.SYS_NAME, regs.RAX, 
+        regs.RDI, regs.RSI, 
+        regs.RDX, regs.R10, 
+        regs.R8, regs.R9,);
 
 
-})
+    res.render('confirmation', {details: regs, errors: []});
+});
+
+app.get('/SYSCALLS', async (req, res) => {
+    const data = await pullSYSCALLS();
+    console.log(data);
+    res.render('SYSCALLS', {details: data, errors: []});
+});
 
 
 
